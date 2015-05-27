3e035db80f171d6cff363c96603e2caa7260e841
xeriscape <fb@consideredharmful.info>
2015-05-27 12:04:24 +0200

    Implement automated search
    Search can use the same files as twtscrp

--- automate_ln_search.py
+++ automate_ln_search.py
@@ -18,11 +18,20 @@ def main():
 			query = x[0].replace("\n","").replace("\r","")
 			since = x[1].replace("\n","").replace("\r","")
 			until = x[2].replace("\n","").replace("\r","")
-			crsor = x[3].replace("\n","").replace("\r","")
-			is_realtime = True
+			
+			query_items = query.split(" ")
+			reassembled_query = []
+			for item in query_items:
+				if ((item[0] != "-") and (item != "lang:en")):
+					reassembled_query.append(item)
+					
+			new_query = " ".join(reassembled_query)
 		
 			print "\n--------- Executing search ",x," ---------\n"
-			websearch_scrape.execute_search(query, since, until, crsor, is_realtime)
+			search_string = "DATE(>={0} and <={1}) and HEADLINE({2}) not SUBJECT(patent or advert!)".format(since, until, new_query)
+			#search_string, access_url, username, password, starting_page
+			#lexisnexis_analysis.conduct_analysis(search_string, location, username, password, 1)
+			print "Query: ",search_string, location, username, password, "1"
 			
 		except (KeyboardInterrupt, SystemExit):
 			print "Process aborted."
